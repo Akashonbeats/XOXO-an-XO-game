edc1ea5930d5416106763b40f09569f473b5c8d3
Akashonbeats <akash.s1204@gmail.com>
2024-04-11 22:42:53 +0530

    Initial Commit

---
+++ XOXO/XOXOApp.swift
@@ -0,0 +1,17 @@
+//
+//  XOXOApp.swift
+//  XOXO
+//
+//  Created by Akash Sampath on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct XOXOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
